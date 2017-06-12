b8b910e3450c4bd4471f93337a21cb01b1e7092c
Biryukov Egor <birykovegor@gmail.com>
2017-06-12 20:54:53 +0300

    A instruction translator

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
@@ -12,20 +13,43 @@ vector<string> read(string file) {
     
     if (myfile.is_open()) {
         while (getline(myfile, line)) {
-            cout << line << endl;
             lines.push_back(line);
         }
-    
         myfile.close();
     } else {
         cout << "Unable to open file"; 
     }
 
-  return lines;
+    return lines;
+}
+
+string translate_a_instruction(string& instruction) {
+    string addr_str = instruction.substr(1, instruction.size() - 1);
+    int addr_dec = stoi(addr_str);
+    cout << addr_dec << endl;
+    string binary = "0" + bitset<15>(addr_dec).to_string();
+    cout << binary << endl;
+    return binary;
+}
+
+string translate(string assembly) {
+    char& first = assembly.front();
+
+    if (first == '@') {
+        return translate_a_instruction(assembly);
+    } else {
+        cout << "This is a C instruction" << endl;
+        return assembly;
+    }
 }
 
 
 int main () {
-  read("hello.txt");
+  vector<string> lines = read("hello.txt");
+  
+  for (string& s : lines) {
+      translate(s);
+  }
+  
   return 0;
 }
